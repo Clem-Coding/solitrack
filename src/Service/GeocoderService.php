e28042c96116303f255c396ae0d7b758998a5b7d
Clem-Coding <clementine.blondeau@gmail.com>
2025-10-28 12:47:05 +0100

    fix : add try/catch and adaptive cache to prevent page crashes

--- src/Service/GeocoderService.php
+++ src/Service/GeocoderService.php
@@ -27,34 +27,42 @@ class GeocoderService
     public function geocode(string $city, string $postcode): ?array
     {
         $query = $postcode . ' ' . $city;
+        $cacheKey = 'geocode_' . md5($query);
 
-        return $this->cache->get('geocode_' . md5($query), function (ItemInterface $item) use ($query) {
-            $item->expiresAfter(86400);
+        return $this->cache->get($cacheKey, function (ItemInterface $item) use ($query) {
+            try {
+                $url = 'https://api-adresse.data.gouv.fr/search/';
+                $response = $this->client->request('GET', $url, [
+                    'query' => [
+                        'q' => $query,
+                        'limit' => 1,
+                    ]
+                ]);
 
-            $url = 'https://api-adresse.data.gouv.fr/search/';
+                $data = $response->toArray();
 
-            $response = $this->client->request('GET', $url, [
-                'query' => [
-                    'q' => $query,
-                    'limit' => 1,
-                ]
-            ]);
+                if (!empty($data['features'])) {
+                    $coords = $data['features'][0]['geometry']['coordinates'];
+                    $city = $data['features'][0]['properties']['city'] ?? 'Ville inconnue';
+                    $postcode = $data['features'][0]['properties']['postcode'] ?? '';
 
-            $data = $response->toArray();
+                    $item->expiresAfter(30 * 24 * 3600); // 30 days cache if successful
+                    return [
+                        'lat' => $coords[1],
+                        'lon' => $coords[0],
+                        'city' => $city,
+                        'postcode' => $postcode,
+                    ];
+                }
 
-            if (!empty($data['features'])) {
-                $coords = $data['features'][0]['geometry']['coordinates'];
-                $city = $data['features'][0]['properties']['city'] ?? 'Ville inconnue';
-                $postcode = $data['features'][0]['properties']['postcode'] ?? '';
-                return [
-                    'lat' => $coords[1],
-                    'lon' => $coords[0],
-                    'city' => $city,
-                    'postcode' => $postcode,
-                ];
+                // If no data, short cache to avoid too many requests
+                $item->expiresAfter(5 * 60); // 5 min
+                return null;
+            } catch (\Throwable $e) {
+                // Short cache on error to prevent a crash and retry later
+                $item->expiresAfter(5 * 60); // 5 min
+                return null;
             }
-
-            return null;
         });
     }
 }
